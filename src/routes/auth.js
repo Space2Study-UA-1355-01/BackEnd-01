0ff7b72ea9ccf282869510d7081e1750374eec1b
VoroninaMaria <m.voronina0803@gmail.com>
2025-04-18 00:26:07 +0300

    Add logic to confirm email

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,12 +3,16 @@ const router = require('express').Router()
 const asyncWrapper = require('~/middlewares/asyncWrapper')
 const validationMiddleware = require('~/middlewares/validation')
 const langMiddleware = require('~/middlewares/appLanguage')
+const user = require('~/models/user');
 
 const authController = require('~/controllers/auth')
 const signupValidationSchema = require('~/validation/schemas/signup')
 const { loginValidationSchema } = require('~/validation/schemas/login')
 const resetPasswordValidationSchema = require('~/validation/schemas/resetPassword')
 const forgotPasswordValidationSchema = require('~/validation/schemas/forgotPassword')
+const jwt = require('jsonwebtoken');
+
+
 
 router.post(
   '/signup',
@@ -31,5 +35,36 @@ router.patch(
   langMiddleware,
   asyncWrapper(authController.updatePassword)
 )
+
+router.get('/confirm-email', async (req, res) => {
+  const token = req.query.token;
+  
+
+  if (!token) return res.status(400).json({ message: 'Tocken not found' });
+
+  try {
+    console.log("JWT_CONFIRM_SECRET:", jwt.verify(token, process.env.JWT_CONFIRM_SECRET));
+    const decoded = jwt.verify(token, process.env.JWT_CONFIRM_SECRET);
+    
+
+    const findUser = await user.findById(decoded.id);
+    if (!findUser) return res.status(404).json({ message: 'User not found' });
+
+    findUser.isEmailConfirmed = true;
+    await findUser.save();
+
+    res.send(`
+      <html>
+        <body>
+          <h2>✅ Email success!</h2>
+        </body>
+      </html>
+    `);
+  } catch (err) {
+    console.error('Token verification error:', err); 
+    res.status(400).send('❌ Invalid or expired token');
+  }
+});
+
 
 module.exports = router
