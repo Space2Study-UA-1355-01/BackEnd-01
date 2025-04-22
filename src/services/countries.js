ca23638e3f2e48305fc6c7b61e23138db032174f
VoroninaMaria <m.voronina0803@gmail.com>
2025-04-22 23:04:30 +0300

    Add mock countries

--- src/services/countries.js
+++ src/services/countries.js
@@ -3,14 +3,30 @@ const axios = require('axios');
 const API_KEY = 'api-key';
 const BASE_URL = 'https://api.countrystatecity.in/v1';
 
+// const getCountries = async () => {
+//   try {
+//     const response = await axios.get(`${BASE_URL}/countries`, {
+//       headers: {
+//         'X-CSCAPI-KEY': API_KEY,
+//       },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     throw new Error('Error with getting countries');
+//   }
+// };
+
+
 const getCountries = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/countries`, {
-      headers: {
-        'X-CSCAPI-KEY': API_KEY,
-      },
-    });
-    return response.data;
+    const response = [
+      { name: 'Ukraine', iso2: 'UA' },
+      { name: 'Poland', iso2: 'PL' },
+      { name: 'Germany', iso2: 'DE' },
+      { name: 'France', iso2: 'FR' },
+      { name: 'Italy', iso2: 'IT' },
+    ];
+    return response;
   } catch (error) {
     throw new Error('Error with getting countries');
   }
