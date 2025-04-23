1e42f162b905da27829600ba1303981a13995669
VoroninaMaria <m.voronina0803@gmail.com>
2025-04-23 13:42:47 +0300

    Add swagger and tests

---
+++ src/test/unit/services/country.spec.js
@@ -0,0 +1,55 @@
+const countryService = require('~/services/countries');
+const axios = require('axios');
+
+jest.mock('axios');
+
+describe('countryService', () => {
+
+  describe('getCountries', () => {
+    it('should return a list of countries from the external API', async () => {
+      const mockCountries = [
+        {
+          id: 233,
+          name: 'United States',
+          iso2: 'US',
+          iso3: 'USA',
+          phonecode: '1',
+          capital: 'Washington',
+          currency: 'USD',
+          native: 'United States',
+          emoji: '🇺🇸',
+        },
+        {
+          id: 244,
+          name: 'Canada',
+          iso2: 'CA',
+          iso3: 'CAN',
+          phonecode: '1',
+          capital: 'Ottawa',
+          currency: 'CAD',
+          native: 'Canada',
+          emoji: '🇨🇦',
+        }
+      ];
+
+      axios.get.mockResolvedValue({ data: mockCountries });
+
+      const result = await countryService.getCountries();
+
+      expect(axios.get).toHaveBeenCalledWith('https://api.countrystatecity.in/v1/countries', {
+        headers: { 'X-CSCAPI-KEY': 'OWRxZlBwaXpIWXlYN3loZlB6cm1hdkhpWHVNeUthR1lFVlU3S21vWA==' }
+      });
+      expect(result).toEqual(mockCountries);
+    });
+
+    it('should throw an error if the external API fails', async () => {
+        axios.get.mockRejectedValueOnce(new Error('Error fetching countries'));
+      
+        try {
+          await countryService.getCountries();
+        } catch (error) {
+          expect(error.message).toBe('Error with getting countries');
+        }
+      });
+  });
+});
