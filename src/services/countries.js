f963c4f4421a292fd31b7a03e6353b942b9ae765
VoroninaMaria <m.voronina0803@gmail.com>
2025-04-23 12:34:15 +0300

    Add api key for countries query

--- src/services/countries.js
+++ src/services/countries.js
@@ -1,32 +1,16 @@
 const axios = require('axios');
 
-const API_KEY = 'api-key';
+const API_KEY = 'OWRxZlBwaXpIWXlYN3loZlB6cm1hdkhpWHVNeUthR1lFVlU3S21vWA==';
 const BASE_URL = 'https://api.countrystatecity.in/v1';
-
-// const getCountries = async () => {
-//   try {
-//     const response = await axios.get(`${BASE_URL}/countries`, {
-//       headers: {
-//         'X-CSCAPI-KEY': API_KEY,
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     throw new Error('Error with getting countries');
-//   }
-// };
-
 
 const getCountries = async () => {
   try {
-    const response = [
-      { name: 'Ukraine', iso2: 'UA' },
-      { name: 'Poland', iso2: 'PL' },
-      { name: 'Germany', iso2: 'DE' },
-      { name: 'France', iso2: 'FR' },
-      { name: 'Italy', iso2: 'IT' },
-    ];
-    return response;
+    const response = await axios.get(`${BASE_URL}/countries`, {
+      headers: {
+        'X-CSCAPI-KEY': API_KEY,
+      },
+    });
+    return response.data;
   } catch (error) {
     throw new Error('Error with getting countries');
   }
