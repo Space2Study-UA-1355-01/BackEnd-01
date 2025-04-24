ed2f9e819048811f468017cf98bf35960722a2d1
VoroninaMaria <m.voronina0803@gmail.com>
2025-04-24 12:41:58 +0300

    Add filtration and pagination to countries and cities

--- src/controllers/countries.js
+++ src/controllers/countries.js
@@ -1,8 +1,19 @@
 const countriesService = require('../services/countries');
 
 const fetchCountries = async (req, res) => {
+  const { search = '', page = 1, limit = 20 } = req.query;
+
   try {
-    const countries = await countriesService.getCountries();
+    const countries = await countriesService.getCountries({
+      search,
+      page: parseInt(page),
+      limit: parseInt(limit),
+    });
+
+    if (!countries || countries.data.length === 0) {
+      return res.status(404).json({ message: `No countries found` });
+    }
+
     res.json(countries);
   } catch (error) {
     console.error(error);
@@ -12,16 +23,23 @@ const fetchCountries = async (req, res) => {
 
 const fetchCitiesByCountry = async (req, res) => {
   const { countryCode } = req.params;
+  const { search = '', page = 1, limit = 20 } = req.query;
 
   if (!countryCode) {
     return res.status(400).json({ message: 'Country code is required' });
   }
 
   try {
-    const cities = await countriesService.getCitiesByCountryIso2(countryCode);
-    if (!cities || cities.length === 0) {
+    const cities = await countriesService.getCitiesByCountryIso2(countryCode, {
+      search,
+      page: parseInt(page),
+      limit: parseInt(limit),
+    });
+
+    if (!cities || cities.data.length === 0) {
       return res.status(404).json({ message: `No cities found for country code '${countryCode}'` });
     }
+
     res.json(cities);
   } catch (error) {
     console.error(error);
