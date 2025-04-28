7eead22b4cba0cc92b4aecdee2c84d6c3a3cff15
yrudikov <89663152+yrudikov@users.noreply.github.com>
2025-04-28 22:37:21 +0200

    Close: #254 Implemented user photo upload with Cloudinary integration (#13)
    * added cloudinary
    
    * Some updates
    
    * Added swagger doc
    
    * Added validation and some updates
    
    * Updated uploadPhoto controller
    
    * Update uploadImage - saving imgs to user folders

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,12 +1,19 @@
 const multer = require('multer')
 
-const storage = multer.memoryStorage()
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 10 * 1024 * 1024,
-    fieldNameSize: 100
-  }
-})
+const defaultStorage = multer.memoryStorage()
 
-module.exports = upload
+const defaultLimits = {
+  fileSize: 10 * 1024 * 1024, // 10MB
+  fieldNameSize: 100
+}
+const createUpload = ({ storage = defaultStorage, limits = defaultLimits, fileFilter } = {}) => {
+  return multer({
+    storage,
+    limits,
+    fileFilter
+  })
+}
+
+const upload = createUpload()
+
+module.exports = { upload, createUpload }
