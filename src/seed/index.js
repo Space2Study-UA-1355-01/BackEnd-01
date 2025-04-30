584c3a8bf60de958d68f07c54f693ea5a5bd0791
VoroninaMaria <m.voronina0803@gmail.com>
2025-05-01 01:49:46 +0300

    Add seed with drop command

--- src/seed/index.js
+++ src/seed/index.js
@@ -7,13 +7,18 @@ const SeedUsers = require('~/seed/seedUsers');
 const SeedCategories = require('~/seed/seedCategories');
 const SeedSubjects = require('~/seed/seedSubjects');
 const logger = require('~/logger/logger'); 
-
+const shouldDropDatabase = process.argv.includes('--drop');
 const MONGO_URI = process.env.MONGODB_URL;
 
 const seedAll = async () => {
   try {
     await mongoose.connect(MONGO_URI);
     console.log('Connected to MongoDB');
+    
+    if (shouldDropDatabase) {
+      await mongoose.connection.dropDatabase();
+      console.log('Database dropped successfully!');
+    }
 
     await checkUserExistence();
     await SeedUsers.createUsers();
